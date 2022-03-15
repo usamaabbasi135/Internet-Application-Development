8df34a582f9816b0bb93ef2049b88e32aa5e6df0
usamaabbasi135 <usamahafeez.abbasi1234@gmail.com>
2022-03-16 00:32:12 +0500

    Created a simple program that would generate the random number after every X amount of seconds and would write it to the html file

---
+++ Lab 01/Print Random Number/Print Random Number/printRandomNumberInHtmlFile.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<fstream>
+#include <Windows.h>
+
+using namespace std;
+void createFile();
+int main() {
+
+    int seconds;
+    cout << "Enter value of seconds. The program would generate new numbers after X seconds: " << endl;
+    cin >> seconds;
+    int milli_seconds = seconds * 1000;
+    
+    while (1)
+    {
+        createFile();
+        Sleep(milli_seconds);
+    }
+
+
+    return 0;
+    
+}
+
+void createFile() {
+
+
+    ofstream file("RandomNumbers.html");           //creating an HTML file 
+    file << "The random numbers are: </br> " << endl;    //Writting initial line to the file
+    srand(time(0));   //This function would help in generating new random number every time the program runs
+    int randomNumber[5]; //Creating an array to store random numbers
+
+    //This loop would generate and add the random numbers to the array and file as well
+    for (int i = 0; i < 5; i++) {
+        randomNumber[i] = rand();
+        file << randomNumber[i] << endl << "</br>";
+    }
+
+    //Closing the file
+    file.close();
+}
